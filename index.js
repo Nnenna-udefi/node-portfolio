430a80802d9326253a40fba358d84098959be36b
Nnenna Udefi <miriamudefi@gmail.com>
2022-10-27 23:26:58 +0000

    Redirect

--- index.js
+++ index.js
@@ -20,28 +20,10 @@ router.get('/contact',function(req,res){
 
 //add the router
 app.use('/', router);
-// let router = require('./routes');
 
-// // serve your css as static
-// app.use(express.static(__dirname));
-
-// app.get("/", (req, res) => {
-  
-//   res.sendFile(__dirname + "/index.html");
-// });
-// const myPortfolio = function (req, res) {
-//     fs.readFile(__dirname + "/index.html")
-//         .then(contents => {
-//             res.setHeader("Content-Type", "text/html");
-//             res.writeHead(200);
-//             res.end(contents);
-//         })
-//    .catch(err => {
-//             res.writeHead(500);
-//             res.end(err);
-//             return;
-//         });
-// };
+app.get('*', function(req, res) {
+    res.redirect('/');
+});
 
 app.listen(3000, () => {
   console.log('server started');
